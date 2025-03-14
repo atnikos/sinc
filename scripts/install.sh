934738c697600d61dd7041eea3b819fc5174ce32
Nikos Athanasiou <nathanasiou@tue.mpg.de>
2025-03-14 19:41:13 +0100

    fix small change

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,9 +8,10 @@ source $PWD/sinc-env/bin/activate
 
 $PWD/sinc-env/bin/pip install --upgrade pip setuptools
 
-$PWD/sinc-env/bin/pip install torch torchvision torchaudio
+$PWD/sinc-env/bin/pip install "torch==2.0.1" "torchvision==0.15.2" "torchaudio==2.0.2" --index-url https://download.pytorch.org/whl/cu118
+
 # torch 2.0.0 cu117
 
-$PWD/sinc-env/bin/pip install pytorch-lightning==2.0.1
-$PWD/sinc-env/bin/pip install torchmetrics==0.11.4
+$PWD/sinc-env/bin/pip install "pytorch-lightning==2.0.1"
+$PWD/sinc-env/bin/pip install "torchmetrics==0.11.4"
 $PWD/sinc-env/bin/pip install -r requirements.txt
